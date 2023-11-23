a1c8519cd8822ded585c544ef40b68aff0662af6
Christian <purin213@hotmail.com>
2023-11-23 15:53:57 +0900

    fix file routing and autoload

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
 spl_autoload_extensions(".php");
-spl_autoload_extensions(function ($class) {
+spl_autoload_register(function ($class) {
     $base_dir = __DIR__ . '/src/';
     $file = $base_dir . str_replace('\\', '/', $class) . '.php';
     if (file_exists($file)){
-    require $file;
+        require $file;
     }
 });
 
